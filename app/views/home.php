e5ddd7d289ef70244eafd28c887212c37df297bf
arturokkboss33 <gomezchavez.arturo@gmail.com>
2017-02-08 19:33:16 +0100

    Complete entire description of the diver pose dataset 2

--- app/views/home.php
+++ app/views/home.php
@@ -51,23 +51,23 @@
 
       <h2 style="font-size:24px">Diver body pose estimation</h2>
       <p class="justify">
-      The AUV (Autonomous Underwater Vehicle) needs to face the diver from the front in order to communicate with him/her through a tablet attached to the AUV. It is also the optimal position to monitor the diver's behavior and overall well-being e.g. breathing pattern, equipment position, etc.
+      The AUV (Autonomous Underwater Vehicle) needs to face the diver from the front in order to communicate with him/her through an attached tablet. It is also the optimal position to monitor the diver's behavior and overall well-being e.g. breathing pattern, equipment position, etc.
       </p> 
       <p class="justify">
       To keep the AUV in front of the diver at all times, the diver wears a system of inertial sensors in the suit that transmits his pose acoustically (DiverNet) <a href="http://ieeexplore.ieee.org/document/7133640/" target="_blank">[1]</a>. However, acoustic communication's bandwith and tranmission rate (~5s) prevents the system from having this information in real time. For this reason, diver pose estimation methods based on stereo images were developed [2] and this database was created. 
       </p>
       <p class="justify">
-      To collect data divers were asked to perform three tasks in front of the AUV: turn 360° horizontally (chest pointing downwards) and vertically, clockwise and anticlockwise, and swim freely. For the latter, the AUV was operated manually to follow the diver. Ground thruth is provided by the inertial sensor located in the diver's chest, which logs the data to an underwater tablet directly hooked to the DiverNet or to an on-land computer through an optic fiber cable. Three data collection experiments were done in an open-sea environment in Biograd na Moru, Croatia; and in an indoor pool at Brodarski Institute, Zagreb, Croatia. 
+      To collect the data, divers were asked to perform three tasks in front of the AUV: turn 360° horizontally (chest pointing downwards) and vertically, clockwise and anticlockwise, and swim freely. For the latter, the AUV was operated manually to follow the diver. Ground thruth is provided by the inertial sensor located in the diver's chest, which logs the data to an underwater tablet directly hooked to the DiverNet or to an on-land computer through an optic fiber cable. Three data collection experiments were done in an open-sea environment in Biograd na Moru, Croatia; and in an indoor pool at Brodarski Institute, Zagreb, Croatia. 
       </p>  
  
       <p class="justify">
-      As follows we describe the files provided for each dataset:
+      We describe the files provided for each dataset as follows:
  
       <ul class="text" id="introul">
 
       	<li>Sample pointcloud of the diver (view only)</li>
       	<li>Zip file with stereo images (rectified)</li>
-	<li>ROS bagfile of the recorded experiment, with the following topics:</li>
+	<li>ROS bagfile of the recorded experiment wich publishes the following topics:</li>
 
 	<ul class=text>
 		<li><i>/stereo_camera/rect/{left,right}/image_raw</i> - Rectified color images</li>
@@ -80,8 +80,8 @@
       </ul> 
       </p>
 
-      <p class="justify">
-      It is important to mention that the bagfile shows the recording of the complete experiment whereas the provided stereo images are the ones particularly used for the developed algorithms, they only include images where the diver appears on the field of view and from which sufficiently dense point clouds could be generated. For a more in detail explanation of our methodology please refer to the <i>Publications</i> section [2].
+      <p class="justify">i
+      It is important to mention that the bagfile shows the recording of the complete experiment whereas the provided stereo images are the ones particularly used for the developed algorithms. They only include images where the diver appears on the field of view and from which sufficiently dense point clouds could be generated. For a more detailed explanation of our methodology please refer to the <i>Publications</i> section [2].
       </p>
 
     </div>
